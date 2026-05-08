ddc7ece8e02b61c0d3614fb7777a858a296e57f1
RagniNagpal <ragninagpal@gmail.com>
2026-05-08 22:18:16 +0530

    all codes in src/

--- src/ques2/max_depth_binary_tree.java
+++
@@ -1,7 +0,0 @@
-﻿// LeetCode #104 - Maximum Depth of Binary Tree
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    }
-}
--- src/ques2/single_number.java
+++
@@ -1,9 +0,0 @@
-class single_number {
-    public int singleNumber(int[] nums) {
-        int result =0;
-        for(int n : nums){
-            result^=n;
-        }
-        return result ;
-    }
-}
