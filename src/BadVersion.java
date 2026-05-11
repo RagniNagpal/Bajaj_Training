c47e990fb80f5bbbec2257bb45290ab2fb51b7c6
RagniNagpal <ragninagpal@gmail.com>
2026-05-11 21:14:55 +0530

    all codes in src/

---
+++ src/BadVersion.java
@@ -0,0 +1,24 @@
+
+class First_Bad_Version {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    // Mock method for testing
+    private boolean isBadVersion(int version) {
+        return version >= 4;
+    }
+}
---
+++ src/Diameter.java
@@ -0,0 +1,34 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Diameter_Binary_Tree {
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = dfs(node.left);
+        int rightHeight = dfs(node.right);
+
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
